a11b2ac78bbed4009b38ef3ca77036b0879f23b1
Muhammad Bux Brohi <muhammadbuxbrohi222@gmail.com>
2026-05-07 17:24:27 +0500

    utlas code descation

--- src/index.js
+++ src/index.js
@@ -7,8 +7,14 @@ import connectDB from "./db/index.js";
 dotenv.config({ path: "./.env" });
 
 connectDB();
-
-
+.than(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    }
+})
+    .config((err) => {
+        console.log("mongodb connection failed !", err);
+    })
 
 
 
